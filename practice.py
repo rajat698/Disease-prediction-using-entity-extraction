24d3aa2fd99bf8e00cab7ddeebe1ffdf380b62da
rajatcodesx <67538489+rajat698@users.noreply.github.com>
2022-11-10 15:34:28 -0700

    First training test

--- practice.py
+++ practice.py
@@ -1,8 +1,5 @@
-import spacy
-
-nlp = spacy.load("en_core_web_sm")
-raw_text=open('dataset.txt', 'r')
-
-text1= nlp(raw_text)
-for word in text1.ents:
-    print(word.text,word.label_)
+a = [('Agoraphobia', 'DISEASE'), ('cancer', 'DISEASE'), ('Aphasia', 'DISEASE'), ('Aphasia', 'DISEASE'), ('Appendicitis', 'DISEASE'), ('Brucellosis', 'DISEASE'), ('Cervicitis', 'DISEASE'), ('Epilepsy', 'DISEASE'), ('Hemophilia', 'DISEASE'), ('Kleptomania', 'DISEASE'), ('Kleptomania', 'DISEASE'), ('microcephaly', 'DISEASE'), ('XLA', 'DISEASE'), ('XLA', 'DISEASE'), ('XLA', 'DISEASE'), ('Waldenstrom', 'DISEASE'), ('cancer', 'DISEASE'), ('Saliva', 'DISEASE'), ('food', 'DISEASE'), ('Waldenstrom', 'DISEASE'), ('Tachycardia', 'DISEASE'), ('Tachycardia', 'DISEASE'), ('Tuberculosis', 'DISEASE'), ('AIDS', 'DISEASE')]
+sum = 0
+for i in a:
+    sum += 1
+print(sum)
