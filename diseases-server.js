f2cc7d5163f41cb295e8c3ff1c5a230f247b0aaf
dbiegan <dbiegan@asu.edu>
2022-11-28 19:34:45 -0700

    query and update working

--- diseases-server.js
+++ diseases-server.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 const http = require('http');
 
+var path_to_project = 'C:\\Users\\biega\\Disease-prediction-using-entity-extraction';
+var path_to_jena = 'C:\\Users\\biega\\apache-jena-4.6.1\\lib';
+var option;
+
 http.createServer((request, response) => {
-    if (request.method === 'GET' && request.url === '/') {
+    if (request.method === 'GET' && (request.url === '/' || request.url === '/?')) {
         fs.readFile('index.html', function (error, index) {
             if (error) {
                 response.writeHead(404);
@@ -13,7 +17,104 @@ http.createServer((request, response) => {
             }
             response.end();
         });
-    }
+    } else if (request.method === 'POST' && request.url === '/query') {
+        let body = [];
+        request.on('data', (chunk) => {
+          body.push(chunk);
+        }).on('end', () => {
+            body = Buffer.concat(body).toString();
+            let formData = body.split('&');
+            // Write the CMD script
+            option = formData[1].split('=')[1].toLowerCase();
+            let criteria = formData[0].split('=')[1];
+            criteria = criteria.replace(/\+/g, " ");
+            console.log("option = " + option);
+            console.log("criteria = " + criteria);
+            let script = writeScript();
+            fs.writeFile('criteria.txt', criteria, function (err) {
+                fs.writeFile('diseases.cmd', script, function (err) {
+                    // Invoke SPARQL program
+                    var exec = require('child_process').exec;
+                    exec('start diseases.cmd', function (error, stdout, stderr) {
+                        if (error !== null) {
+                            console.log("exec error: " + error);
+                        } else {
+                            // Read the result from the output file
+                            fs.readFile('result.txt', 'utf-8', function (err, data) {
+                                if (err != null) console.log("Error reading output.txt!");
+                                else {
+                                    response.writeHead(200, { 'Content-Type': 'text/html' });
+                                    let result = '<!DOCTYPE html><html lang="en" xmlns="http://www.w3.org/1999/xhtml"><head><meta charset="utf-8" />'
+                                    + '<title>Diseases Database and Entity Extraction</title><body style="background-color:lightskyblue"><div style="text-align:center">'
+                                    + '<h2>Results</h2><form action="/" method="get">'
+                                    + data + '<br>'
+                                    + '<div div style="padding-top:10px"><input type="submit" value="Back" /></div></form></div></body></html>'
+                                    response.write(result);
+                                }
+                                response.end();
+                            });
+                        }   
+                    });
+                });
+            });
+        });
+    } else if (request.method === 'POST' && request.url === '/update') {
+        let body = [];
+        request.on('data', (chunk) => {
+          body.push(chunk);
+        }).on('end', () => {
+            body = Buffer.concat(body).toString();
+            body = body.substring(5);
+            body = body.replace(/\+/g, " ");
+            body = body.replace(/%3A/g, ":");
+            body = body.replace(/%3C/g, "<");
+            body = body.replace(/%2F/g, "/");
+            body = body.replace(/%23/g, "#");
+            body = body.replace(/%3E/g, ">");
+            body = body.replace(/%0D/g, "\r");
+            body = body.replace(/%0A/g, "\n");
+            body = body.replace(/%7B/g, "{");
+            body = body.replace(/%7D/g, "}");
+            console.log("text = " + body);
+            
+            // Write the CMD script
+            option = 'update';
+            updateCommands = '';
+            let script = writeScript();
+            fs.writeFile('diseases.cmd', script, function (err) {
+                // Invoke SPARQL program
+                fs.writeFile('text.txt', body, function (err) {
+                    var exec = require('child_process').exec;
+                    exec('start diseases.cmd', function (error, stdout, stderr) {
+                        fs.readFile('index.html', function (error, index) {
+                            if (error) {
+                                console.log('Content you are looking for not found! :(');
+                            } else {
+                                console.log("Database updated!");
+                            }
+                        });
+
+                    });
+                    response.writeHead(200, { 'Content-Type': 'text/html' });
+                    let result = '<!DOCTYPE html><html lang="en" xmlns="http://www.w3.org/1999/xhtml"><head><meta charset="utf-8" />'
+                        + '<title>Diseases Database and Entity Extraction</title><body style="background-color:lightskyblue"><div style="text-align:center">'
+                        + '<h2>Updating database. Check back later!</h2><form action="/" method="get">'
+                        + '<div div style="padding-top:10px"><input type="submit" value="Back" /></div></form></div></body></html>'
+                    response.write(result);
+                    response.end();
+                });
+            });
+        });
+      }
 }).listen(8080, function() {
     console.log('diseases-server listening on port 8080...');
 });
+
+function writeScript() {
+    let script = 'cd ' + path_to_project + '\n'
+    + 'javac -cp ".;' + path_to_jena + '\\*" MedicalConditions.java\n'
+    + 'java -cp ".;' + path_to_jena + '\\*"'
+    + ' MedicalConditions ' + option + '\n' 
+    + 'exit'
+    return script;
+}
